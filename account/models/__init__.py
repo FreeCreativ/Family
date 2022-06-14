0534b697cf6853cbe384c340c0ae4d79636e1742
freecreativ <okaforjonathant@outlook.com>
2022-06-14 14:00:20 +0100

    Updated the dashboard

---
+++ account/models/__init__.py
@@ -0,0 +1,6 @@
+from .account import UserAccount, UserDetail
+from .education import Education
+from .occupation import Occupation
+from .email import AdditionalEmail
+from .disease import GeneticDisease
+from .phone import PhoneRecord
