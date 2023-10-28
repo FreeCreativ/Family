668c80671f129d803230a5aff6aa193f2cb55dc8
Okafor <okaforjonathant@outlook.com>
2023-10-28 14:07:09 +0100

    Nice

--- Family/urls.py
+++ Family/urls.py
@@ -19,8 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('django.contrib.auth.urls')),
-                  path('', include('home.urls')),
-                  path('', include('account.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('home.urls')),
+    path('', include('account.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
