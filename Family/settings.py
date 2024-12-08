633904c1af8cbbdd74457016778fa9092cee7077
FreeCreativ <53322795+FreeCreativ@users.noreply.github.com>
2024-12-08 17:18:51 +0100

    Update settings.py

--- Family/settings.py
+++ Family/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6p5p+tihy#dlg9qp4b%v+l4dwswnbgox=ns@+l51&%rp7&0w%i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['familyregistry.onrender.com']
 
 # Application definition
 
@@ -81,17 +81,6 @@ WSGI_APPLICATION = 'Family.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'familyregistry',
-#         'USER': 'freecreativ',
-#         'PASSWORD': 'XJG8ZpKvRvPGZbFkazw8Qil31lAj2VsG12345678',
-#         'HOST': 'https://dashboard.render.com/d/dpg-ckekvfedkmrc738b33dg-a',
-#         'PORT': '5432'
-#     }
-# }
 
 # DATABASES = {
 #     'default': {
@@ -166,5 +155,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 import dj_database_url
 DATABASES={
-    'default': dj_database_url.parse("postgres://freecreativ:XJG8ZpKvRvPGZbFkazw8Qil31lAj2VsG@dpg-ckekvfedkmrc738b33dg-a/familyregistry")
+    'default': dj_database_url.parse("postgresql://freecreativ:7yUTumpAVjDmLb3OkMLRHJpMH9zrCumh@dpg-ctasad23esus739cuql0-a/family_lie4")
 }
